40308933994e098af1e31e5b9830d966a8f9756e
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-08-30 15:28:06 -0400

    index view that lists abstract titles

--- app/abstracts/views.py
+++ app/abstracts/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Version
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the abstracts index.")
+    version_list = Version.objects.all()
+    template = loader.get_template("index.html")
+    context = {
+        'version_list': version_list,
+    }
+    return HttpResponse(template.render(context, request))
+    
