17d2a5e2203e95b2c38c0230d48f6505cc52a452
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-08-30 09:51:13 -0400

    define string methods for models

--- app/abstracts/models.py
+++ app/abstracts/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Work(models.Model):
     work_id = models.IntegerField(primary_key=True)
 
+    def __str__(self):
+        return self.work_id
+
 class Version(models.Model):
     work_id = models.ForeignKey(Work, on_delete=models.CASCADE)
     title = models.CharField(max_length = 500)
@@ -12,3 +15,6 @@ class Version(models.Model):
     year = models.IntegerField()
     state = models.CharField(max_length = 255)
     full_text = models.CharField(max_length = 50000)
+
+    def __str__(self):
+        return self.title
