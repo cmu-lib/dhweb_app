01d6579da2a84ec2c92ec88beb4f0994bdb78c5f
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-08-30 16:52:54 -0400

    try to create a version detail page
    not functioning yet, really

--- app/abstracts/urls.py
+++ app/abstracts/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('version/<int:version_id>/', views.version_detail, name='version_detail')
 ]
--- app/abstracts/views.py
+++ app/abstracts/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import get_object_or_404, render
 
 from .models import Version
 
 # Create your views here.
 def index(request):
     version_list = Version.objects.all()
-    template = loader.get_template("index.html")
-    context = {
-        'version_list': version_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'version_list': version_list}
+    return render(request, 'index.html', context)
     
+def version_detail(request, version_id):
+    version = get_object_or_404(Version, pk = version_id)
+    context = {'version': Version}
+    return render(request, "version_detail.html", context)
