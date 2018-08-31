81364fb87e9ba5aae19cabea8d9f5e0fbf7fd174
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-08-31 15:37:35 -0400

    link to version detail pages from index list

--- app/abstracts/views.py
+++ app/abstracts/views.py
@@ -3,15 +3,15 @@ from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
 
-from .models import Version
+from .models import Version, Tag
 
 # Create your views here.
 def index(request):
-    version_list = Version.objects.all()
+    version_list = Version.objects.all()[:10]
     context = {'version_list': version_list}
     return render(request, 'index.html', context)
     
 def version_detail(request, version_id):
     version = get_object_or_404(Version, pk = version_id)
-    context = {'version': Version}
+    context = {'version': version}
     return render(request, "version_detail.html", context)
