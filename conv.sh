e30a3d4729feef80d54c425fddb48e11ad16b553
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-08-31 13:46:21 -0400

    temporarily transliterate all data to ASCII
    avoid encoding problems until we can get the models and fixtures working

---
+++ conv.sh
@@ -0,0 +1,7 @@
+SRCDATA="/Users/mlincoln/Development/dh_conferences/adho_fixture.json"
+
+TGTDATA="/Users/mlincoln/Development/dh_web/app/abstracts/fixtures/adho_fixture.json"
+
+iconv -c -f utf-8 -t ascii//TRANSLIT < $SRCDATA | sed -e 's/"/\"/g' > $TGTDATA
+
+# CP $SRCDATA $TGTDATA
