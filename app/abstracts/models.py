ac9511012feeb67861bb40a948f4f9cac4f89c54
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-08-31 14:11:33 -0400

    make most fields optional

--- app/abstracts/models.py
+++ app/abstracts/models.py
@@ -13,22 +13,22 @@ class Work(models.Model):
 
 class Tag(models.Model):
     tag_id = models.IntegerField(primary_key=True)
-    title = models.CharField(max_length=100)
-    type = models.CharField(max_length=100)
-    StartDate = models.IntegerField()
-    EndDate = models.IntegerField()
+    title = models.CharField(max_length=100, null=True)
+    type = models.CharField(max_length=100, null=True)
+    StartDate = models.CharField(max_length=100, null=True)
+    EndDate = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return self.title
 
 class Version(models.Model):
-    work_id = models.ForeignKey(Work, on_delete=models.CASCADE)
-    title = models.CharField(max_length = 500)
-    type = models.CharField(max_length = 255)
-    year = models.IntegerField()
-    state = models.CharField(max_length = 255)
-    full_text = models.CharField(max_length = 50000)
-    tags = models.ForeignKey(Tag, on_delete=models.CASCADE, null=True)
+    work_id = models.ForeignKey(Work, on_delete=models.CASCADE, related_name='versions')
+    title = models.CharField(max_length=500, null=True)
+    type = models.CharField(max_length=255, null=True)
+    year = models.IntegerField(null=True)
+    state = models.CharField(max_length=255, null=True)
+    full_text = models.CharField(max_length=50000, null=True)
+    tags = models.ManyToManyField(Tag, related_name="versions")
 
     def __str__(self):
         return self.title
@@ -37,62 +37,62 @@ class Version(models.Model):
         return datetime.date.today().year - self.year
 
 class Department(models.Model):
-    department = models.CharField(max_length = 100)
+    department = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return self.department
 
 class Institution(models.Model):
-    institution = models.CharField(max_length = 100)
-    country = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-
-class FirstName(models.Model):
-    first_name = models.CharField(max_length = 100)
-    start_date = models.DateField()
-    end_date = models.DateField()
-
-    def __str__(self):
-        return self.first_name
-
-class LastName(models.Model):
-    last_name = models.CharField(max_length=100)
-    start_date = models.DateField()
-    end_date = models.DateField()
-
-    def __str__(self):  
-        return self.last_name
+    name = models.CharField(max_length=100, null=True)
+    country = models.CharField(max_length=100, null=True)
+    city = models.CharField(max_length=100, null=True)
 
 class Gender(models.Model):
     gender = models.CharField(max_length = 100)
-    start_date = models.DateField()
-    end_date = models.DateField()
+    start_date = models.DateField(null=True)
+    end_date = models.DateField(null=True)
 
     def __str__(self):
         return self.gender
 
 class Author(models.Model):
     author_id = models.IntegerField(primary_key=True)
-    first_names = models.ForeignKey(FirstName, on_delete=models.CASCADE)
-    last_names = models.ForeignKey(LastName, on_delete=models.CASCADE)
-    genders = models.ForeignKey(Gender, on_delete=models.CASCADE)
+    genders = models.ManyToManyField(Gender, related_name='authors')
 
     def __str__(self):
         return str(self.author_id)
 
+class FirstName(models.Model):
+    first_name = models.CharField(max_length = 100)
+    start_date = models.DateField(null=True)
+    end_date = models.DateField(null=True)
+    author_id = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='first_names')
+
+    def __str__(self):
+        return self.first_name
+
+class LastName(models.Model):
+    last_name = models.CharField(max_length=100)
+    start_date = models.DateField(null=True)
+    end_date = models.DateField(null=True)
+    author_id = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='last_names')
+
+    def __str__(self):  
+        return self.last_name
+
 class Authorship(models.Model):
-    author_id = models.ForeignKey(Author, on_delete=models.CASCADE)
-    version_id = models.ForeignKey(Version, on_delete=models.CASCADE)
-    authorship_order = models.IntegerField()
+    author_id = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='authorships')
+    version_id = models.ForeignKey(Version, on_delete=models.CASCADE, related_name='authorships')
+    authorship_order = models.IntegerField(default=1)
 
 class DepartmentMembership(models.Model):
     author_id = models.ForeignKey(Author, on_delete=models.CASCADE)
     department_id = models.ForeignKey(Department, on_delete=models.CASCADE)
-    start_date = models.DateField()
-    end_date = models.DateField()
+    start_date = models.DateField(null=True)
+    end_date = models.DateField(null=True)
 
 class InstitutionMembership(models.Model):
-    author_id = models.ForeignKey(Author, on_delete=models.CASCADE)
-    institution_id = models.ForeignKey(Department, on_delete=models.CASCADE)
-    start_date = models.DateField()
-    end_date = models.DateField()
+    author_id = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='institution_memberships')
+    institution_id = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='author_memberships')
+    start_date = models.DateField(null=True)
+    end_date = models.DateField(null=True)
