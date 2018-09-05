74a8f5ae6e237906b5cb33dfb435c85a32d30ca7
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-09-05 10:12:31 -0400

    Created Work detail page
    Closes #5

--- app/abstracts/urls.py
+++ app/abstracts/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('version/<int:version_id>/', views.version_detail, name='version_detail')
+    path('version/<int:version_id>/', views.version_detail, name='version_detail'),
+    path('<int:work_id>/', views.work_detail, name = 'work_detail')
 ]
--- app/abstracts/views.py
+++ app/abstracts/views.py
@@ -3,15 +3,21 @@ from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
 
-from .models import Version, Tag
+from .models import Version, Tag, Work
 
 # Create your views here.
 def index(request):
     version_list = Version.objects.all()[:10]
     context = {'version_list': version_list}
     return render(request, 'index.html', context)
-    
+
 def version_detail(request, version_id):
     version = get_object_or_404(Version, pk = version_id)
     context = {'version': version}
     return render(request, "version_detail.html", context)
+
+def work_detail(request, work_id):
+    work = get_object_or_404(Work, pk = work_id)
+    version_list = Version.objects.filter(work_id=work)
+    context = {'work': work, 'version_list': version_list}
+    return render(request, "work_detail.html", context)
