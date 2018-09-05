dc7901ff4de804f36faa0c5f8755422cddefb690
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-09-05 14:25:49 -0400

    Create tag list
    Closes #12

--- app/abstracts/urls.py
+++ app/abstracts/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:work_id>/', views.work_detail, name = 'work_detail'),
+    path('tag', views.IndexView.as_view(), name = 'tag_list'),
     path('tag/<int:pk>', views.DetailView.as_view(), name = "tag_detail")
 ]
