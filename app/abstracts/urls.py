3d78bf7b8fc1e1c67542d0925950d35b8c7be20b
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-09-05 14:19:40 -0400

    remove version detail pages
    closes #16

--- app/abstracts/urls.py
+++ app/abstracts/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('version/<int:version_id>/', views.version_detail, name='version_detail'),
     path('<int:work_id>/', views.work_detail, name = 'work_detail'),
     path('tag/<int:pk>', views.DetailView.as_view(), name = "tag_detail")
 ]
--- app/abstracts/views.py
+++ app/abstracts/views.py
@@ -11,14 +11,9 @@ class DetailView(generic.DetailView):
     template_name = 'tag_detail.html'
 
 def index(request):
-    version_list = Version.objects.all()[:10]
-    context = {'version_list': version_list}
+    version_list = Work.objects.all()[:10]
+    context = {'work_list': work_list}
     return render(request, 'index.html', context)
-
-def version_detail(request, version_id):
-    version = get_object_or_404(Version, pk = version_id)
-    context = {'version': version}
-    return render(request, "version_detail.html", context)
 
 def work_detail(request, work_id):
     work = get_object_or_404(Work, pk = work_id)
