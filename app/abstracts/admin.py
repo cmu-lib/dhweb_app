cad3cafa0d5d99107424937b632d2ec6e5b73826
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-09-07 15:25:10 -0400

    de-register old models from admin interface
    we'll reinstate them soon

--- app/abstracts/admin.py
+++ app/abstracts/admin.py
@@ -1,16 +1,5 @@
 from django.contrib import admin
 
-from .models import Work, Tag, Version, Department, Institution, FirstName, LastName, Gender, Author, Authorship, DepartmentMembership, InstitutionMembership
+from .models import Version
 
-admin.site.register(Work)
-admin.site.register(Tag)
 admin.site.register(Version)
-admin.site.register(Department)
-admin.site.register(Institution)
-admin.site.register(FirstName)
-admin.site.register(LastName)
-admin.site.register(Gender)
-admin.site.register(Author)
-admin.site.register(Authorship)
-admin.site.register(DepartmentMembership)
-admin.site.register(InstitutionMembership)
