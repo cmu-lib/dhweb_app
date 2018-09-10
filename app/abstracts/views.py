50e74d420f8e4bf2bffb93ed1c255d90812749e5
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-09-10 10:42:26 -0400

    make work views generic
    Closes #15

--- app/abstracts/views.py
+++ app/abstracts/views.py
@@ -2,41 +2,39 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
-from django.views import generic
+from django.views.generic import DetailView, ListView
 
 from .models import Version, Tag, Work, Author
 
-class TagView(generic.DetailView):
+class TagView(DetailView):
     model = Tag
     template_name = 'tag_detail.html'
 
-class TagList(generic.ListView):
+class TagList(ListView):
     context_object_name = 'tag_list'
     template_name = 'tag_list.html'
 
     def get_queryset(self):
         return Tag.objects.all()
 
-class WorkList(generic.ListView):
+class WorkList(ListView):
     context_object_name = 'work_list'
     template_name = 'index.html'
 
     def get_queryset(self):
         return Work.objects.all()[:10]
 
-class AuthorView(generic.DetailView):
+class WorkView(DetailView):
+    model = Work
+    template_name = 'work_detail.html'
+
+class AuthorView(DetailView):
     model = Author
     template_name = 'author_detail.html'
 
-class AuthorList(generic.ListView):
+class AuthorList(ListView):
     context_object_name = 'author_list'
     template_name = 'author_list.html'
 
     def get_queryset(self):
         return Author.objects.all()[:10]
-
-def work_detail(request, work_id):
-    work = get_object_or_404(Work, pk = work_id)
-    version_list = work.versions.all()
-    context = {'work': work, 'version_list': version_list}
-    return render(request, "work_detail.html", context)
