fbeb6cbb60143348826338f5628a6e5ee848a2f5
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-09-11 16:14:16 -0400

    don't require trailing slash on work detail urls

--- app/abstracts/urls.py
+++ app/abstracts/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.WorkList.as_view(), name='index'),
-    path('<int:pk>/', views.WorkView.as_view(), name = 'work_detail'),
+    path('<int:pk>', views.WorkView.as_view(), name = 'work_detail'),
     path('tag', views.TagList.as_view(), name = 'tag_list'),
     path('tag/<int:pk>', views.TagView.as_view(), name = "tag_detail"),
     path('author', views.AuthorList.as_view(), name = 'author_list'),
