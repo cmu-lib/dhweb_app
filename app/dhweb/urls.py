a39cf729c6d5d3266b65cc36dda550be6f5fba88
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-11-09 11:35:44 -0500

    abstracts start at /
    Clsoes #50

--- app/dhweb/urls.py
+++ app/dhweb/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('abstracts/', include('abstracts.urls')),
+    path('', include('abstracts.urls')),
     path('admin/', admin.site.urls),
 ]
