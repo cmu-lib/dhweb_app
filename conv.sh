085a9a9d74ba0d1107c2a28221eda46dad9391ba
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-12-17 09:17:51 -0500

    update script to match new directory structure

--- conv.sh
+++ conv.sh
@@ -1,6 +1,6 @@
-SRCDATA="/Users/mlincoln/Development/dh_conferences/adho_fixture.json"
+SRCDATA="/Users/mlincoln/Development/dh_abstracts/dh_conferences/adho_fixture.json"
 
-TGTDATA="/Users/mlincoln/Development/dh_web/app/abstracts/fixtures/adho_fixture.json"
+TGTDATA="/Users/mlincoln/Development/dh_abstracts/dh_web/app/abstracts/fixtures/adho_fixture.json"
 
 iconv -c -f utf-8 -t ascii//TRANSLIT < $SRCDATA | sed -e 's/"/\"/g' > $TGTDATA
 
