0db44dfe7f6ace5028b3e43cb27062c93f6a83b5
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-12-20 09:46:29 -0500

    properly set up nginx

--- app/dhweb/settings.py
+++ app/dhweb/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = "/vol/static_files"
 
 # Using pygraphviz to create ERDs of models
 
