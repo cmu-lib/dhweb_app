c2ca2f897403a788038dc6c94a108042d61984df
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-12-20 08:57:23 -0500

    specify allowed hosts to local and dc-vm2

--- app/dhweb/settings.py
+++ app/dhweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("DH_SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "dc-vm2-rhel.andrew.cmu.edu"]
 
 
 # Application definition
