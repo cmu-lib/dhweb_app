3a4d16902fb7732accdcf66a8f8da9f429be72d7
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-12-30 17:21:19 -0500

    paginate work list while preserving query attributes

---
+++ app/abstracts/templatetags/query_filters.py
@@ -0,0 +1,13 @@
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag
+def url_replace(request, field, value):
+
+    dict_ = request.GET.copy()
+
+    dict_[field] = str(value)
+
+    return dict_.urlencode()
