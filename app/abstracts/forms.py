9112214ec809fdfd7305fc6354035a282ab79dc3
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-12-31 10:39:35 -0500

    get simple work filter working

--- app/abstracts/forms.py
+++ app/abstracts/forms.py
@@ -1,2 +1,27 @@
 from django import forms
 
+from .models import Author, Authorship, Conference, Institution, Topic, Keyword, Work
+
+
+class WorkFilter(forms.Form):
+    # submission_type = forms.ChoiceField(
+    #     choices=Work.objects.values_list("submission_type").distinct()
+    # )
+    conference = forms.ModelChoiceField(
+        queryset=Conference.objects.filter(works__state="ac").distinct(),
+        required=False,
+        help_text="Works submitted to a particular conference",
+    )
+    institution = forms.ModelChoiceField(
+        queryset=Institution.objects.filter(
+            affiliations__asserted_by__work__state="ac"
+        ).distinct(),
+        required=False,
+        help_text="Works submitted with at least one author belonging to that institution.",
+    )
+    keywords = forms.ModelChoiceField(
+        queryset=Keyword.objects.filter(works__state="ac").distinct(), required=False
+    )
+    topics = forms.ModelChoiceField(
+        queryset=Topic.objects.filter(works__state="ac").distinct(), required=False
+    )
--- app/dhweb/settings.py
+++ app/dhweb/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "bootstrap4",
+    "crispy_forms",
 ]
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
