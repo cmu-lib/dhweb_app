7b04e519ad777cd78f8d7c835e6b05e6009584e7
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-12-31 13:15:46 -0500

    remove institution views/urls/templates

--- app/abstracts/urls.py
+++ app/abstracts/urls.py
@@ -9,10 +9,4 @@ urlpatterns = [
     path("authors", views.AuthorList.as_view(), name="author_list"),
     path("authors/<int:pk>", views.AuthorView.as_view(), name="author_detail"),
     path("conferences", views.ConferenceList.as_view(), name="conference_list"),
-    path("institutions", views.InstitutionList.as_view(), name="institution_list"),
-    path(
-        "institutions/<int:pk>",
-        views.InstitutionView.as_view(),
-        name="institution_detail",
-    ),
 ]
