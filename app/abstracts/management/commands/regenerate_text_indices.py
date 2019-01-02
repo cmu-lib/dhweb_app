092380d4be5704e9334e531af922e789aed3cbe9
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-01-02 15:20:22 -0500

    management command to repopulate FTS indices

---
+++ app/abstracts/management/commands/regenerate_text_indices.py
@@ -0,0 +1,24 @@
+"""
+Full text indices for various models need to be calculated by concatenating
+multiple fields into one SearchVector field. This only needs to be done
+periodically, so it is included here as a management command.
+"""
+
+from django.core.management.base import BaseCommand
+from django.contrib.postgres.search import SearchVector
+from abstracts.models import Work, Appellation
+
+
+class Command(BaseCommand):
+    help = "Reindexes the text SearchVectors for appropriate models"
+
+    def handle(self, *args, **kwargs):
+
+        print("Updating index for Appellations...", end="", flush=True)
+        Appellation.objects.update(search_text=SearchVector("first_name", "last_name"))
+        print("done.")
+
+        print("Updating index for Works...", end="", flush=True)
+        Work.objects.update(search_text=SearchVector("title", "full_text"))
+        print("done.")
+
