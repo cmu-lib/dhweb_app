e4f84216760a5f6d8c7857b516565c33d967b894
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-01-07 14:23:49 -0500

    force transliteration of UTF8

--- conv.sh
+++ conv.sh
@@ -2,6 +2,6 @@ SRCDATA="/Users/mlincoln/Development/dh_abstracts/dh_conferences/adho_fixture.js
 
 TGTDATA="/Users/mlincoln/Development/dh_abstracts/dh_web/app/abstracts/fixtures/adho_fixture.json"
 
-iconv -c -f utf-8 -t ascii//TRANSLIT < $SRCDATA | sed -e 's/"/\"/g' > $TGTDATA
+iconv -c -f utf-8 -t utf-8//TRANSLIT < $SRCDATA | sed -e 's/"/\"/g' > $TGTDATA
 
 # CP $SRCDATA $TGTDATA
