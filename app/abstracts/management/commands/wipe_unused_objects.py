c571f3b46ce0ac612471f9030d04a66737a5aa08
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-01-07 15:32:39 -0500

    command to wipe unreferenced objects
    closes #124

---
+++ app/abstracts/management/commands/wipe_unused_objects.py
@@ -0,0 +1,15 @@
+from django.core import management
+from django.core.management.base import BaseCommand
+from abstracts import models
+
+
+class Command(BaseCommand):
+    help = "Wipe all unfreferenced Appellations, Authors, Affiliations, Institutions, Countries, Keywords, and Topics"
+
+    def handle(self, *args, **options):
+        models.Author.objects.filter(authorships__isnull=True).delete()
+        models.Affiliation.objects.filter(asserted_by__isnull=True).delete()
+        models.Institution.objects.filter(affiliations__isnull=True).delete()
+        models.Country.objects.filter(institutions__isnull=True).delete()
+        models.Keyword.objects.filter(works__isnull=True).delete()
+        models.Topic.objects.filter(works__isnull=True).delete()
