403b1cad55f0634d55ec1cda7c54c12d23ef0700
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-04-28 17:37:03 -0400

    split empty view home page cases

--- app/abstracts/tests/test_views.py
+++ app/abstracts/tests/test_views.py
@@ -27,25 +27,24 @@ from abstracts.models import (
 )
 
 
-class EmptyViewTest(TestCase):
+class EmptyHomeViewTest(TestCase):
     """
     Test pages when the database is empty
     """
 
-    def test_blank_db(self):
-        """
-        If no series exist, display appropriate message
-        """
-
+    def test_home_blank(self):
         home_response = self.client.get(reverse("home_view"))
         self.assertEqual(home_response.status_code, 200)
 
+    def test_work_blank(self):
         work_response = self.client.get(reverse("work_list"))
         self.assertEqual(work_response.status_code, 200)
 
+    def test_author_blank(self):
         author_response = self.client.get(reverse("author_list"))
         self.assertEqual(author_response.status_code, 200)
 
+    def test_conference_blank(self):
         conference_response = self.client.get(reverse("conference_list"))
         self.assertEqual(conference_response.status_code, 200)
 
