c8f759e3e61a74adfec3392b6d0e6f37f0643235
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-04-29 11:32:16 -0400

    remove old vm from ALLOWED_HOSTS

--- app/dhweb/settings.py
+++ app/dhweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("DH_SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "***REMOVED***"]
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
