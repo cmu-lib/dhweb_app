4953c8d6de6e8702821f66ea825564e209556fea
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-04-29 14:06:48 -0400

    smarter uniqueness checks
    closes #144
    closes #143

--- app/abstracts/tests/test_views.py
+++ app/abstracts/tests/test_views.py
@@ -29,6 +29,10 @@ from abstracts.models import (
 from abstracts.forms import WorkFilter
 
 
+def is_list_unique(x):
+    return len(x) == len(set(x))
+
+
 class EmptyListViewTest(TestCase):
     """
     Test pages when the database is empty
@@ -62,26 +66,27 @@ class AuthorListViewTest(TestCase):
         author_list_response = self.client.get(reverse("author_list"))
         self.assertEqual(author_list_response.status_code, 200)
 
-    def test_length(self):
-        author_list_response = self.client.get(reverse("author_list"))
-        self.assertEqual(
-            len(
-                set(
-                    author_list_response.context["author_list"].values_list(
-                        "pk", flat=True
-                    )
-                )
-            ),
-            2,
-        )
-
     def test_query_filtered_count(self):
         author_list_response = self.client.get(reverse("author_list"))
-        self.assertEqual(author_list_response.context["filtered_authors_count"], 2)
+        self.assertEqual(
+            author_list_response.context["filtered_authors_count"],
+            len(author_list_response.context["author_list"]),
+        )
 
     def test_query_total_count(self):
         author_list_response = self.client.get(reverse("author_list"))
-        self.assertEqual(author_list_response.context["available_authors_count"], 2)
+        self.assertIsInstance(
+            author_list_response.context["available_authors_count"], int
+        )
+
+    def test_unique(self):
+        author_list_response = self.client.get(reverse("author_list"))
+        self.assertTrue(is_list_unique(author_list_response.context["author_list"]))
+
+    def test_all_accepted(self):
+        author_list_response = self.client.get(reverse("author_list"))
+        for author in author_list_response.context["author_list"]:
+            self.assertGreaterEqual(author.works.filter(state="ac").count(), 1)
 
 
 class AuthorDetailViewTest(TestCase):
@@ -97,32 +102,56 @@ class AuthorDetailViewTest(TestCase):
         )
         self.assertEqual(author_detail_response.status_code, 200)
 
-    def test_series(self):
+    def test_works_unqiue_in_series(self):
         author_detail_response = self.client.get(
             reverse("author_detail", kwargs={"pk": 1})
         )
-        self.assertEqual(len(author_detail_response.context["split_works"]), 1)
+        for series in author_detail_response.context["split_works"]:
+            self.assertTrue(is_list_unique(series["works"]))
 
-    def test_works(self):
+    def test_appellations_unique(self):
         author_detail_response = self.client.get(
             reverse("author_detail", kwargs={"pk": 1})
         )
-        self.assertEqual(len(set(author_detail_response.context["split_works"][0])), 2)
+        self.assertTrue(
+            is_list_unique(
+                [
+                    d["appellation"]
+                    for d in author_detail_response.context["appellation_assertions"]
+                ]
+            )
+        )
 
-    def test_appellations(self):
+    def test_affiliations_unique_institutions(self):
         author_detail_response = self.client.get(
             reverse("author_detail", kwargs={"pk": 1})
+        )
+        self.assertTrue(
+            is_list_unique(
+                [
+                    d["institution"]
+                    for d in author_detail_response.context["affiliation_assertions"]
+                ]
+            )
+        )
+
+    def test_affiliations_each_has_value(self):
+        author_detail_response = self.client.get(
+            reverse("author_detail", kwargs={"pk": 1})
+        )
+        for assertion in author_detail_response.context["affiliation_assertions"]:
+            self.assertGreaterEqual(len(assertion["works"]), 1)
+            self.assertTrue(is_list_unique(assertion["works"]))
+
+    def test_unaccepted_author(self):
+        """
+        Authors with only submitted papers should not have pages
+        """
+        unaccepted_author_detail_response = self.client.get(
+            reverse("author_detail", kwargs={"pk": 3})
         )
         self.assertEqual(
-            len(author_detail_response.context["appellation_assertions"]), 2
-        )
-
-    def test_affiliations(self):
-        author_detail_response = self.client.get(
-            reverse("author_detail", kwargs={"pk": 1})
-        )
-        self.assertEqual(
-            len(author_detail_response.context["affiliation_assertions"]), 2
+            len(unaccepted_author_detail_response.context["split_works"]), 0
         )
 
 
@@ -137,20 +166,12 @@ class WorkListViewTest(TestCase):
         author_list_response = self.client.get(reverse("author_list"))
         self.assertEqual(author_list_response.status_code, 200)
 
-    def test_length(self):
-        work_list_response = self.client.get(reverse("work_list"))
-        self.assertEqual(
-            len(
-                set(
-                    work_list_response.context["work_list"].values_list("pk", flat=True)
-                )
-            ),
-            2,
-        )
-
     def test_query_filtered_count(self):
         work_list_response = self.client.get(reverse("work_list"))
-        self.assertIsInstance(work_list_response.context["filtered_works_count"], int)
+        self.assertEqual(
+            work_list_response.context["filtered_works_count"],
+            len(work_list_response.context["work_list"]),
+        )
 
     def test_query_total_count(self):
         work_list_response = self.client.get(reverse("work_list"))
