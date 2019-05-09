5440ae790ca8c06f8f1b3385f5d21780f60b1355
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-05-09 10:01:28 -0400

    correctly check envvar for DEBUG_STATUS

--- app/dhweb/settings.py
+++ app/dhweb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("DH_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG_STATUS")
+DEBUG = os.environ.get("DEBUG_STATUS") == "True"
 
 ALLOWED_HOSTS = ["localhost", os.environ.get("ALLOWED_HOSTS")]
 
