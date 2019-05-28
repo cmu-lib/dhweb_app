498ea4e51f7e90b69435af390056ac7a24eafbe0
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-05-28 15:42:24 -0400

    exclude sessions from backsup
    clsoes #251

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker-compose exec dh-web python manage.py dumpdata -e contenttypes --indent 2 -o /vol/data/backups/backup.json
+docker-compose exec dh-web python manage.py dumpdata -e contenttypes -e sessions --indent 2 -o /vol/data/backups/backup.json
 cd /vol/data/backups
 git add .
 DATE=`date +%Y-%m-%d`
