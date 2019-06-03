575a48f0e317dcdc9dff1327c6057e4ef91bba1c
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-06-03 10:26:12 -0400

    export data views and regen text indices nightly

--- backup.sh
+++ backup.sh
@@ -5,6 +5,11 @@ set -e
 source /vol/dh_web/.env
 
 /home/mlincoln/.local/bin/docker-compose -f /vol/dh_web/docker-compose.yml --project-directory /vol/dh_web/ exec -T dh-web python manage.py dumpdata -e contenttypes -e sessions --indent 2 -o /vol/data/backups/backup.json
+
+/home/mlincoln/.local/bin/docker-compose -f /vol/dh_web/docker-compose.yml --project-directory /vol/dh_web/ exec -T dh-web python manage.py export_all_csv
+
+/home/mlincoln/.local/bin/docker-compose -f /vol/dh_web/docker-compose.yml --project-directory /vol/dh_web/ exec -T dh-web python manage.py regenerate_text_indices
+
 cd /vol/data/backups
 /usr/bin/git add .
 DATE=`date +%Y-%m-%d`
