2d2c39ba4cf71ecda6b88d586436687e98fc0b9d
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2020-04-02 16:12:20 -0400

    try to define site id

--- dh_abstracts/app/dhweb/settings.py
+++ dh_abstracts/app/dhweb/settings.py
@@ -144,3 +144,4 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.environ.get("EMAIL_HOST")
 EMAIL_PORT = os.environ.get("EMAIL_PORT", 25)
 DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_ADDRESS")
+SITE_ID = os.environ.get("ALLOWED_HOSTS", "localhost")
