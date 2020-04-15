ed3422be929b7313a4d1bd7350f89993540a8e10
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2020-04-15 19:44:34 -0400

    enable account management urls

--- dh_abstracts/app/dhweb/urls.py
+++ dh_abstracts/app/dhweb/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path("", include("abstracts.urls")),
     path("", include("password_reset.urls")),
     path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
