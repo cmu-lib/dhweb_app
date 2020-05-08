44fa2e7d7622c6f9c329e2cccf36ec47d4c1fca8
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2020-05-08 13:48:42 -0400

    just hardcode the postgres host name
    its hardcoded in the docker-compose file anyway

--- dh_abstracts/app/dhweb/settings.py
+++ dh_abstracts/app/dhweb/settings.py
@@ -112,7 +112,7 @@ DATABASES = {
         "NAME": os.environ["POSTGRES_DB"],
         "USER": os.environ["POSTGRES_USER"],
         "PASSWORD": os.environ["POSTGRES_PASSWORD"],
-        "HOST": os.environ["POSTGRES_HOST"],
+        "HOST": "postgres",
         "PORT": 5432,
     }
 }
