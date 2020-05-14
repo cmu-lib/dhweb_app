208ad4b6963af121559fb15a97137042f24b60c3
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2020-05-14 17:16:01 -0400

    correctly access settings

--- dh_abstracts/app/abstracts/management/commands/export_tables.py
+++ dh_abstracts/app/abstracts/management/commands/export_tables.py
@@ -90,7 +90,7 @@ class Command(BaseCommand):
                 f"{settings.DATA_OUTPUT_PATH}/{settings.DENORMALIZED_WORKS_NAME}.zip"
             )
             csv_path = f"{tdir}/{settings.DENORMALIZED_WORKS_NAME}.csv"
-            header_names = [h.name for h in settings.DENORMALIZED_HEADERS]
+            header_names = [h["name"] for h in settings.DENORMALIZED_HEADERS]
 
             with ZipFile(zip_path, "w") as dat_zip:
                 with open(csv_path, "w") as csv_file:
