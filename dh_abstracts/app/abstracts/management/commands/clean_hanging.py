dbfcbf2902855a1b05f10cff41e0af87e4708094
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2020-09-21 10:38:13 -0400

    management command to clean hanging objects

---
+++ dh_abstracts/app/abstracts/management/commands/clean_hanging.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+from abstracts import models
+from django.db import transaction
+from django.db.models import Q
+
+
+class Command(BaseCommand):
+    help = "Erase hanging records"
+
+    def handle(self, *args, **options):
+        models.Conference.objects.get(id=23).works.all().delete()
+        models.Author.objects.exclude(authorships__isnull=False).all().delete()
+        models.Affiliation.objects.exclude(asserted_by__isnull=False).all().delete()
+        models.Institution.objects.exclude(
+            Q(affiliations__asserted_by__isnull=False) | Q(conferences__isnull=False)
+        ).all().delete()
+        models.Keyword.objects.exclude(works__isnull=False).all().delete()
+        models.Appellation.objects.exclude(asserted_by__isnull=False).all().delete()
+
